be91708e2fb4c9f4aabba2058b6842c7583a194f
Brayan Gonzalez <brayan.a.gonzalez@gmail.com>
2017-12-13 16:31:57 -0300

    Se agregan ciudades al seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+#Cuidades definidas en el sistema.
+City.create(name: "Santiago (CL)")
+City.create(name: "Zurich (CH)")
+City.create(name: "Auckland (NZ)")
+City.create(name: "Sydney (AU)")
+City.create(name: "Londres (UK)")
+City.create(name: "Georgia (USA)")
