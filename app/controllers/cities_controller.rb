43b932388b2ec337c9cdcd297c9342db481a8195
Brayan Gonzalez <brayan.a.gonzalez@gmail.com>
2017-12-14 02:24:10 -0300

    Se quitan datos que no eran necesarios en la respuesta json

--- app/controllers/cities_controller.rb
+++ app/controllers/cities_controller.rb
@@ -1,7 +1,7 @@
 class CitiesController < ApplicationController
 
     def index
-        render json: City.all
+        render json: City.all, except: [:id, :created_at, :updated_at]
     end
 
 end
